e88ecf3ab9d122024eedca0ca1d31abc18edcc60
stephenkati <stephenkatuli88@gmail.com>
2023-08-18 13:14:24 +0300

    Create protected route

---
+++ src/components/protetedRoutes.jsx
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types'
+import { useSelector } from 'react-redux'
+import { Navigate } from 'react-router-dom';
+
+const ProtectedRoutes = ({ children }) => {
+  const { isAuthenticated } = useSelector((state) => state.user);
+
+  if (!isAuthenticated){
+    return <Navigate to="/login" />
+  }
+  
+  return children;
+};
+
+ProtectedRoutes.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default ProtectedRoutes;
