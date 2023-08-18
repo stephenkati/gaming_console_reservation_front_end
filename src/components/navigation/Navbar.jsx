16894ca88c0f140e9e6a1fb05b20b49ad18cbe42
Miles Mosweu <tmtmosweu@gmail.com>
2023-08-18 20:35:53 +0200

    Style logo

--- src/components/navigation/Navbar.jsx
+++ src/components/navigation/Navbar.jsx
@@ -10,11 +10,13 @@ const handleMenuClick = () => {
 
 function Navbar() {
   return (
-    // Add with of 20% when the screen is lg or larger
     <nav className="flex justify-between items-center p-4 relative lg:h-screen lg:flex-col lg:justify-space-between lg:items-start lg:pr-0 flex-0.5">
       <MenuIcon onClick={handleMenuClick} />
 
-      <span className="hidden lg:inline-block py-2 px-4">Logo</span>
+      {/* Add an exotic font-family */}
+      <span className="consolehub hidden lg:inline-block py-2 px-4 font-extrabold text-4xl">
+        ConsoleHub
+      </span>
 
       <MobileMenu />
 
