63dd7205445606d44141c64a8602b08e35d89a5b
Miles Mosweu <tmtmosweu@gmail.com>
2023-08-18 18:51:37 +0200

    Integrate logout link into nav

--- src/components/logout.jsx
+++ src/components/logout.jsx
@@ -1,25 +1,16 @@
-import { useNavigate } from 'react-router-dom';
 import { removeToken } from '../utils/localStorage';
+import { Link } from 'react-router-dom';
 
 const Logout = () => {
-  const navigate = useNavigate();
-
   const handleLogout = () => {
     removeToken();
-    navigate('/login');
   };
 
   return (
-    <div>
-      <button
-        type="button"
-        className="bg-secondary text-primary rounded-lg p-2 mt-2"
-        onClick={handleLogout}
-      >
-        Logout
-      </button>
-    </div>
-  )
+    <Link to="/login" onClick={handleLogout}>
+      Logout
+    </Link>
+  );
 };
 
 export default Logout;
--- src/components/navigation/Navbar.jsx
+++ src/components/navigation/Navbar.jsx
@@ -20,18 +20,25 @@ function Navbar() {
       <span className="hidden lg:inline-block">Logo</span>
 
       <div
-        className="absolute top-0 right-0 w-full text-left bg-gray-100 p-4 lg:hidden"
+        className="absolute top-0 right-0 w-full text-left bg-gray-100 p-4 lg:hidden hidden"
         id="menu"
       >
         <MenuIcon onClick={handleMenuClick} />
 
         <ul className="text-center">
           {links.map((link) => (
-            <li key={link.path} className="py-2 hover:bg-gray-200 rounded">
+            <li
+              key={link.path}
+              className="py-2 hover:bg-gray-200 rounded"
+              onClick={handleMenuClick}
+            >
               <Link to={link.path}>{link.text}</Link>
             </li>
           ))}
-          <li>
+          <li
+            className="py-2 hover:bg-gray-200 rounded"
+            onClick={handleMenuClick}
+          >
             <Logout />
           </li>
         </ul>
