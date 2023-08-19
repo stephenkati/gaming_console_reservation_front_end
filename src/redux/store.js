f5a818c81500e5c49778bc65095e3fd75d65b25c
Miles Mosweu <tmtmosweu@gmail.com>
2023-08-19 17:36:17 +0200

    Import reservations reduce into store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './userSlice';
-
+import reservationsReducer from './reservations/reservationsSlice';
 
 const store = configureStore({
   reducer: {
     user: userReducer,
-  }
+    reservations: reservationsReducer,
+  },
 });
 
 export default store;
