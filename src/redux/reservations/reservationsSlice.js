bbec20377a0e169c8c97faa9c5e45bf805c42bd9
Miles Mosweu <tmtmosweu@gmail.com>
2023-08-19 17:33:09 +0200

    Add fecthUserReservations method

--- src/redux/reservations/reservationsSlice.js
+++ src/redux/reservations/reservationsSlice.js
@@ -1,12 +1,37 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
+import customApi from '../../utils/axios';
+import { getToken } from '../../utils/localStorage';
 
 const fetchUserReservations = createAsyncThunk(
   'user/fetchUserReservations',
-  async () => {
+  async (_, thunkAPI) => {
     try {
-      // Fetch user reservations
+      const token = getToken();
+
+      if (!token) {
+        return thunkAPI.rejectWithValue('No authentication token found');
+      }
+
+      const response = await customApi.get('/api/v1/reservations', {
+        headers: {
+          Authorization: token,
+        },
+      });
+
+      const reservations = response.data;
+      console.log(reservations);
+      return reservations;
     } catch (error) {
-      // Handle error
+      if (
+        error.response &&
+        (error.response.status === 401 || error.response.status === 422)
+      ) {
+        return thunkAPI.rejectWithValue(error.response.data.errors[0]);
+      }
+
+      return thunkAPI.rejectWithValue(
+        error.response?.data.errors || 'Unknown error'
+      );
     }
   }
 );
