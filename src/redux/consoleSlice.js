b5fb8610db38a70b667a59aace164cea326cfbe5
naledi-dikgale <naledi22khosi@gmail.com>
2023-08-20 18:21:36 +0200

    Fetch consoles

--- src/redux/consoleSlice.js
+++ src/redux/consoleSlice.js
@@ -1,10 +1,25 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import customApi from '../utils/axios';
+
+const getConsoles = createAsyncThunk(
+  'consoles/getConsoles',
+  async (thunkAPI) => {
+    try {
+      const response = await customApi.get('/api/v1/consoles');
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response?.data.errors || 'Unknown error');
+    }
+  }
+);
 
 const consoleSlice = createSlice({
   name: 'consoles',
   initialState: {
     consoles: [],
     message: null,
+    error: null,
+    isLoading: false,
   },
   reducers: {
     addConsole: (state, action) => {
@@ -14,7 +29,22 @@ const consoleSlice = createSlice({
       state.message = action.payload;
     },
   },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getConsoles.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getConsoles.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.consoles = action.payload.data;
+      })
+      .addCase(getConsoles.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
+      });
+  },
 });
 
 export const { addConsole, setMessage } = consoleSlice.actions;
 export default consoleSlice.reducer;
+export { getConsoles };
