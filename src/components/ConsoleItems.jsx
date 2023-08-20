9afbe9ee659b16be57f0c56ccf75ad40b0c30e62
naledi-dikgale <naledi22khosi@gmail.com>
2023-08-20 21:07:02 +0200

    Add items styling

--- src/components/ConsoleItems.jsx
+++ src/components/ConsoleItems.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getConsoles } from '../redux/consoleSlice';
+import { CiFacebook, CiTwitter, CiInstagram } from 'react-icons/ci';
 
 const ConsoleItems = () => {
   const dispatch = useDispatch();
@@ -10,16 +11,22 @@ const ConsoleItems = () => {
   },[dispatch]);
 
   return (
-    <div>
+    <div className="flex w-full justify-around h-screen items-center p-4 ">
       {consoles.map((console) => (
-        <div key={console.id}>
-          <img src={console.photo} alt={console.name} />
-          <p>{console.name}</p>
-          <p>{console.rental_price}</p>
+        <div key={console.id} className="flex flex-col gap-1 w-60 bg-green-500 h-80 items-center justify-between p-2">
+          <img src={console.photo} alt={console.name} className="w-11/12 h-2/5" />
+          <p className="my-1">{console.name}</p>
+          <div className="flex flex-col gap-1 items-center">
+            <p>{console.description}</p>
+            <div className="flex gap-1">
+              <CiFacebook />
+              <CiTwitter />
+              <CiInstagram />
+            </div>
+          </div>  
         </div>
       ))}
     </div>
   )
 }
-
 export default ConsoleItems;
