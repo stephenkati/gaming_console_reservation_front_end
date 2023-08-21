643c62d855fa7c7adb88cf79891c8201f426dacf
Miles Mosweu <tmtmosweu@gmail.com>
2023-08-21 10:07:23 +0200

    Display console data in view

--- src/components/reservations/ReservationItem.jsx
+++ src/components/reservations/ReservationItem.jsx
@@ -2,17 +2,18 @@ import PropTypes from 'prop-types';
 
 function ReservationItem({ reservation }) {
   return (
-    <div
-      key={reservation.id}
-      className="border border-gray-200 rounded p-4 text-center"
-    >
-      <div className="h-40 w-full bg-gray-200 mb-4 rounded grid place-items-center">
-        <span>Image</span>
+    <div key={reservation.id} className="rounded p-4 text-center">
+      <div className="mb-4 grid place-items-center rounded-full w-52 h-52 mx-auto bg-gray-100">
+        <img
+          src={reservation.console.photo || 'https://via.placeholder.com/150'}
+          alt={reservation.console.name}
+          className="object-cover rounded"
+        />
       </div>
 
       {console.log(reservation)}
       <h2 className="font-bold px-4 pt-2 pb-3 border-b border-b-2 border-dashed border-gray-300 inline-block">
-        Console: {reservation.console_id}
+        {reservation.console.name}
       </h2>
 
       <div className="pt-2">
