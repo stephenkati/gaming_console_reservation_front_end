1c9e285bb52f60164d83e4eea707ded2e56af322
yacoubou-seidou <yacoseidou@gmail.com>
2023-08-21 16:24:27 +0100

    Add link to the console details

--- src/components/ConsoleItems.jsx
+++ src/components/ConsoleItems.jsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getConsoles } from '../redux/consoleSlice';
 import { CiFacebook, CiTwitter, CiInstagram } from 'react-icons/ci';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Link } from 'react-router-dom';
 
 import 'swiper/css';
 import 'swiper/css/navigation';
@@ -38,7 +39,7 @@ const ConsoleItems = () => {
 
   useEffect(() => {
     dispatch(getConsoles())
-  },[dispatch]);
+  }, [dispatch]);
 
   return (
     <div className="flex flex-col w-full justify-around h-screen relative ">
@@ -46,7 +47,7 @@ const ConsoleItems = () => {
         <h1 className="text-4xl font-bold">LATEST CONSOLES</h1>
         <p className="text-lg">Please select a Console</p>
       </div>
-      
+
       <Swiper
         spaceBetween={10}
         slidesPerView={slidesPerView}
@@ -55,9 +56,13 @@ const ConsoleItems = () => {
         className="static w-full flex justify-around px-9"
       >
         {consoles.map((console) => (
-          <SwiperSlide key={console.id} className="flex flex-col gap-1 w-60 h-72 items-center justify-between p-2">
-            <img src={console.photo} alt={console.name} className="w-4/6 h-2/5" />
-              <p className="text-lg font-bold">{console.name}</p>
+          <Link to={`/console/${console.id}`} key={console.id}>
+            <SwiperSlide key={console.id} className="flex flex-col gap-1 w-60 h-72 items-center justify-between p-2">
+              <img src={console.photo} alt={console.name} className="w-4/6 h-2/5" />
+              <Link to={`/console/${console.id}`} key={console.id}>
+
+                <p className="text-lg font-bold">{console.name}</p>
+              </Link>
               <div className="flex flex-col gap-1 items-center">
                 <p className="text-center text-xs px-3">{console.description.slice(0, 60)}...</p>
                 <div className="flex gap-3 text-lg">
@@ -66,7 +71,8 @@ const ConsoleItems = () => {
                   <CiInstagram />
                 </div>
               </div>
-          </SwiperSlide>
+            </SwiperSlide>
+          </Link>
         ))}
 
         <SliderPrevButton />
