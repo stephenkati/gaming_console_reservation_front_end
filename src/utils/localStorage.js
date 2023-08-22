8bf5b64ee286ad5fa63f93ed1909cf96c28253eb
stephenkati <stephenkatuli88@gmail.com>
2023-08-22 07:10:52 +0300

    Create reservation slice

--- src/utils/localStorage.js
+++ src/utils/localStorage.js
@@ -1,11 +1,17 @@
-export const saveToken = (token) => {
+export const saveToken = (token, user) => {
   localStorage.setItem('token', token);
+  localStorage.setItem('user', user);
 };
 
 export const getToken = () => {
   return localStorage.getItem('token');
 }
 
+export const getUser = () => {
+  return localStorage.getItem('user');
+};
+
 export const removeToken = () => {
   localStorage.removeItem('token');
+  localStorage.removeItem('user');
 }
