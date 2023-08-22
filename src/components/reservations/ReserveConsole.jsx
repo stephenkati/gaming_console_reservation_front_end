02195839ee980389619228168e65bdb75152f3a8
stephenkati <stephenkatuli88@gmail.com>
2023-08-22 08:41:52 +0300

    Update input styles

--- src/components/reservations/ReserveConsole.jsx
+++ src/components/reservations/ReserveConsole.jsx
@@ -57,7 +57,7 @@ const ReserveConsole = () => {
 
       <p className="text-sm max-w-md">Try out the latest consoles and a variety of games. Whether it&apos;s for a special occasion or to explore new gaming horizons, renting a console gives you the flexibility, convenience, and excitement you&apos;re looking for. </p>
 
-      <form className="flex flex-wrap w-max gap-1 justify-center">
+      <form className="flex flex-wrap max-w-max gap-1 justify-center">
         <input
           type="text"
           placeholder="City"
@@ -66,7 +66,7 @@ const ReserveConsole = () => {
             setCity(e.target.value);
             setErrors({ ...errors, city: '' });
           }}
-          className={`border rounded-full p-2 w-1/3 focus:outline-none ${
+          className={`border rounded-full p-2 w-1/3 min-w-max focus:outline-none ${
             errors.city ? 'border-red-500' : 'border-secondary'
           }`}
         />
