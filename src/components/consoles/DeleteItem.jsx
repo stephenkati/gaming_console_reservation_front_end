5906d2278312f3d84af3706d5dab6b1b6f6644c9
stephenkati <stephenkatuli88@gmail.com>
2023-08-24 08:59:45 +0300

    Add delete table and styles

--- src/components/consoles/DeleteItem.jsx
+++ src/components/consoles/DeleteItem.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
-import { getConsoles } from '../../redux/consoleSlice';
+import { delConsole, deleteConsole, getConsoles } from '../../redux/consoleSlice';
 
 const DeleteItem = () => {
   const dispatch = useDispatch();
@@ -10,30 +10,31 @@ const DeleteItem = () => {
     dispatch(getConsoles());
   }, [dispatch]);
 
-  const handleDelete = (id) => {
-    console.log("Delete-" + id);
+  const handleDelete = async (console_id) => {
+    await dispatch(deleteConsole(console_id));
+    await dispatch(delConsole(console_id));
   };
 
   return (
-    <div>
-      <h1>DELETE CONSOLE</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Console</th>
-            <th>Action</th>
+    <div className="flex flex-col w-full items-center ">
+      <h1 className="tracking-widest font-bold p-4">DELETE CONSOLE</h1>
+      <table className="w-full max-w-lg">
+        <thead className="w-full m-auto">
+          <tr className="bg-gray-200">
+            <th className="py-2 border-r border-gray-500">Console</th>
+            <th className="py-2">Action</th>
           </tr>
         </thead>
         <tbody>
          {
             consoles.map((console) => (
-              <tr key={console.id}>
-                <td>{console.name}</td>
+              <tr key={console.id} className="w-full text-center even:bg-gray-100 py-2">
+                <td className="py-1 border-r border-gray-500">{console.name}</td>
                 <td>
                   <button
                     type="button"
                     onClick={() => handleDelete(console.id)}
-                    className="bg-red-500 hover:bg-red-600 rounded px-4 py-2 "
+                    className="bg-red-500 hover:bg-red-600 text-white rounded px-4 py-1 my-1 "
                   >
                     Delete
                   </button>
