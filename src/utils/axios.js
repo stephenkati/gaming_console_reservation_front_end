d82ec5f37159fbafcd856237c0927abca7f2df91
stephenkati <stephenkatuli88@gmail.com>
2023-08-25 09:22:43 +0300

    Update baseURL

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const customApi = axios.create({
-  baseURL: 'http://localhost:3000'
+  baseURL: 'https://gaming-console-reservation.onrender.com'
 });
 
 export default customApi;
